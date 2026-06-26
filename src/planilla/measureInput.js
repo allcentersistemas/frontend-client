8f8eecf502769ff7bec90b2725120394ade1bec9
arturoruizromero <sistemas@allcenter.pe>
2026-06-26 17:07:37 -0500

    hola

--- src/planilla/measureInput.js
+++ src/planilla/measureInput.js
@@ -5,7 +5,7 @@ export const MEASURE_FIELDS = ['cantidad', 'largo', 'ancho', 'perforacionCantida
 export const MIN_BOARD_MEASURE = 51
 
 const BOARD_MEASURE_FIELDS = [
-  ['largo', 'Largo'],
+  ['largo', 'ancho'],
   ['ancho', 'Ancho'],
 ]
 
